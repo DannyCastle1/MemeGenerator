4c19fdad02d93cbfb8d67c08da4eacc12c0c6860
DannyCastle1 <castillodaniel1@yahoo.com>
2024-02-16 19:24:13 -0500

    Update App.jsx

--- memegen/src/App.jsx
+++ memegen/src/App.jsx
@@ -4,16 +4,11 @@ import Meme from './components/Meme';
 import './App.css'
 
 function App() {
-
-function clickEvent () {
-  console.log("It worked");
-}
   return (
     <>
     <Header />
     <Meme />
       <div className='container'>
-        <button onClick={clickEvent}>Click me</button>
       </div>
     </>
   )
